e3522d30b882271afd9aaef5a810d4a1644ceb96
Nicholas Hughes <nicholashughes@users.noreply.github.com>
2018-11-21 14:54:29 -0600

    [settings-file] Create a Utils group and add the StartupProjectSettingsUtils files to it.
    This creates a Utils group and adds StartupProjectSettingsUtils.h and .m to it. The
    StartupProjectSettingsUtils will contain settings in code rather than in the Info.plist
    and will provide an alternative way of storing per-environment variables and customizations.

---
+++ ios-startup-guide/ios-startup-guide/Utils/StartupProjectSettingsUtils.h
@@ -0,0 +1,13 @@
+//
+//  StartupProjectSettingsUtils.h
+//  ios-startup-guide
+//
+//  Created by Nicholas Hughes on 11/21/18.
+//  Copyright © 2018 Nicholas Hughes. All rights reserved.
+//
+
+#ifndef StartupProjectSettingsUtils_h
+#define StartupProjectSettingsUtils_h
+
+
+#endif /* StartupProjectSettingsUtils_h */
---
+++ ios-startup-guide/ios-startup-guide/Utils/StartupProjectSettingsUtils.m
@@ -0,0 +1,9 @@
+//
+//  StartupProjectSettingsUtils.m
+//  ios-startup-guide
+//
+//  Created by Nicholas Hughes on 11/21/18.
+//  Copyright © 2018 Nicholas Hughes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>

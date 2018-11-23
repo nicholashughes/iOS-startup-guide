ab843391d5f4a9d365cc44cac239d438bd82c39c
Nicholas Hughes <nicholashughes@users.noreply.github.com>
2018-11-23 00:02:03 -0600

    [swift-bridging] Create a Swift file and its bridging header file.
    Create a Swift file named StartupSwiftExample.swift through Xcode and add it
    to the Utils group. When Xcode asks you to add an Objective-C bridging Header,
    choose 'Yes' and this will create the ios-startup-guide-Bridging-Header.h file.
    
    The name of the bridging header is auto-generated based off the Product Name,
    which is set to the Target Name. It appears in the Utils group simple because
    the Swift file was created in the Utils group.

---
+++ ios-startup-guide/ios-startup-guide/Utils/StartupSwiftExample.swift
@@ -0,0 +1,9 @@
+//
+//  StartupSwiftExample.swift
+//  ios-startup-guide
+//
+//  Created by Nicholas Hughes on 11/22/18.
+//  Copyright © 2018 Nicholas Hughes. All rights reserved.
+//
+
+import Foundation

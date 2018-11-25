f70b4786bceede27c04b9fa8c459a03cf7b50cb6
Nicholas Hughes <nicholashughes@users.noreply.github.com>
2018-11-24 22:35:31 -0600

    [remove-storyboards] Create a 'Controllers' group and create a StartupHomeViewController.
    This creates a group named 'Controllers' for all controllerss and then adds a new
    controller named StartupHomeViewController to that group. This commit contains both the
    new .h and .m files as well as the changes to the project.pbxproj file.

---
+++ ios-startup-guide/ios-startup-guide/Controllers/StartupHomeViewController.h
@@ -0,0 +1,17 @@
+//
+//  StartupHomeViewController.h
+//  ios-startup-guide
+//
+//  Created by Nicholas Hughes on 11/24/18.
+//  Copyright © 2018 Nicholas Hughes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StartupHomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios-startup-guide/ios-startup-guide/Controllers/StartupHomeViewController.m
@@ -0,0 +1,32 @@
+//
+//  StartupHomeViewController.m
+//  ios-startup-guide
+//
+//  Created by Nicholas Hughes on 11/24/18.
+//  Copyright © 2018 Nicholas Hughes. All rights reserved.
+//
+
+#import "StartupHomeViewController.h"
+
+@interface StartupHomeViewController ()
+
+@end
+
+@implementation StartupHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

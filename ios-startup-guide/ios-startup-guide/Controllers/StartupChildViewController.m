39e52e26dbc645e73d9db196451c074623d9c8a2
Nicholas Hughes <nicholashughes@users.noreply.github.com>
2018-11-25 22:39:31 -0600

    [child-controller] Add an 'initWithNibName' function to StartupChildViewController.m.
    Adds the 'initWithNibName' function and specifies the nib to be 'StartupChildView'.

--- ios-startup-guide/ios-startup-guide/Controllers/StartupChildViewController.m
+++ ios-startup-guide/ios-startup-guide/Controllers/StartupChildViewController.m
@@ -14,6 +14,15 @@
 
 @implementation StartupChildViewController
 
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:@"StartupChildView" bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.

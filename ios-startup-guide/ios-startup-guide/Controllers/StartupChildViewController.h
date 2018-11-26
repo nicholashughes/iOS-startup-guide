e2d4acae3e54ae541701d74a033ead79f7529493
Nicholas Hughes <nicholashughes@users.noreply.github.com>
2018-11-25 22:22:01 -0600

    [child-controller] Create a StartupChildViewController.m and .h and add it to the project.
    This creates a StartupChildViewController and adds it to the project.pbxproj file. This
    view controller will control the StartupChildView created earlier.

---
+++ ios-startup-guide/ios-startup-guide/Controllers/StartupChildViewController.h
@@ -0,0 +1,17 @@
+//
+//  StartupChildViewController.h
+//  ios-startup-guide
+//
+//  Created by Nicholas Hughes on 11/25/18.
+//  Copyright © 2018 Nicholas Hughes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StartupChildViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios-startup-guide/ios-startup-guide/Controllers/StartupChildViewController.m
@@ -0,0 +1,32 @@
+//
+//  StartupChildViewController.m
+//  ios-startup-guide
+//
+//  Created by Nicholas Hughes on 11/25/18.
+//  Copyright © 2018 Nicholas Hughes. All rights reserved.
+//
+
+#import "StartupChildViewController.h"
+
+@interface StartupChildViewController ()
+
+@end
+
+@implementation StartupChildViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
